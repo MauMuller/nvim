4a929a34a68994af2d33b73ce2d8635a1f91f926
MauMuller <mauriciomuller2012@gmail.com>
2026-08-17 23:39:26 -0300

    feat: add 'telescope' plugin
    just a fuzzy finder to help to search files

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    keys = {
+	{
+		"<leader>ff",
+		"<cmd>Telescope find_files<cr>"
+	},
+	{
+		"<leader>fg",
+		"<cmd>Telescope live_grep<cr>"
+	},
+    }
+}
