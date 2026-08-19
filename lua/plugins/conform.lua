1718875c8c6729c1df7687778ec3a369922b22be
MauMuller <mauriciomuller2012@gmail.com>
2026-08-18 22:32:53 -0300

    feat: add plugin 'conform' to automatic format file
    using the languages formatters, i can just format the file automatic,
    it's a good way to keep a standart.

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+		formatters_by_ft = {
+			golang = { "golps" },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+	},
+}
