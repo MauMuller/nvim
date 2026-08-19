da63c93d53476683825ae138cbfac970e6ba4e50
MauMuller <mauriciomuller2012@gmail.com>
2026-08-18 22:30:47 -0300

    refactor: add condition to show .gitignore and dot files
    i like to keep this files at list when i need to show it, but, too, i
    added 3 folders to ignore:
    - .git (obvious)
    - vendor (php)
    - node_modules (js)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,37 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    },
-    keys = {
-	{
-		"<leader>ff",
-		"<cmd>Telescope find_files<cr>"
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
-	{
-		"<leader>fg",
-		"<cmd>Telescope live_grep<cr>"
+	version = "*",
+	opts = {
+		defaults = {
+			file_ignore_patterns = {
+				".git",
+				"node_modules",
+				"vendor",
+			},
+		},
+		pickers = {
+			find_files = {
+				hidden = true,
+				no_ignore = true,
+			},
+			live_grep = {
+				hidden = true,
+				no_ignore = true,
+			},
+		},
 	},
-    }
+	keys = {
+		{
+			"<leader>ff",
+			"<cmd>Telescope find_files<cr>",
+		},
+		{
+			"<leader>fg",
+			"<cmd>Telescope live_grep<cr>",
+		},
+	},
 }
