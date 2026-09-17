81bd0d7b1a2a5f0a43ee9e4f7f93c0cb9d3f0079
MauMuller <mauriciomuller2012@gmail.com>
2026-09-16 22:53:18 -0300

    feat: add a nvim-treesitter for sintax sugar
    here is just for a make easier the read at code.

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,15 @@
+return {
+	'nvim-treesitter/nvim-treesitter',
+	lazy = false,
+	build = ':TSUpdate',
+	config = function()
+		require("nvim-treesitter").setup({})
+		vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		vim.wo[0][0].foldmethod = 'expr'
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = { '<filetype>' },
+			callback = function() vim.treesitter.start() end,
+		})
+	end
+}
