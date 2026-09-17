e464bec07eac9a603214fad16b07699eabd6dcb1
MauMuller <mauriciomuller2012@gmail.com>
2026-09-16 22:40:02 -0300

    feat: add mason-lspconfig plugin
    now with blink cmp, mason and lspconfig, i change the mason config file
    to receive the integration.

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,17 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function(_, opts)
+		require("mason").setup()
+
+		local blink_capabilities = require("blink.cmp").get_lsp_capabilities()
+		vim.lsp.config("*", {
+			capabilities = blink_capabilities,
+		})
+
+		require("mason-lspconfig").setup(opts)
+	end
 }
