d8cb1958f76e8da2f505899944d3e1c743875754
MauMuller <mauriciomuller2012@gmail.com>
2026-09-16 22:57:12 -0300

    feat: add tiny-inline-diagnostic plugin for sugar sintax error
    now i have a better way to know about diagnostic errors.

---
+++ lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,20 @@
+return {
+	"rachartier/tiny-inline-diagnostic.nvim",
+	event = "VeryLazy",
+	priority = 1000,
+	config = function()
+		require("tiny-inline-diagnostic").setup({
+			options = {
+				multilines = {
+					enabled = true,
+					always_show = true,
+				},
+				show_source = {
+					enabled = true,
+				},
+				enable_on_insert = true
+			},
+		})
+		vim.diagnostic.config({ virtual_text = false })
+	end,
+}
