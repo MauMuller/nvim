52872891da38fec50965e0899bd9da4dbb98f13f
MauMuller <mauriciomuller2012@gmail.com>
2026-09-16 22:26:48 -0300

    refactor: change the standart configurations
    i added 3 mapps for lsp definitions and added some news config neovim definitions.

--- init.lua
+++ init.lua
@@ -1,4 +1,12 @@
 require("config.lazy")
 
-vim.opt.shiftwidth=2
-vim.opt.tabstop=2
+vim.keymap.set('n', '<leader>cc', vim.lsp.buf.code_action, { desc = "Code Action" })
+vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = "Code Format" })
+vim.keymap.set('n', '<leader>lk', vim.lsp.buf.hover, { desc = "Hover type" })
+
+vim.opt.updatetime = 200
+vim.opt.number = true
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.foldenable = false
